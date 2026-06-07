cd53ca6eb0a51ad822931267b9ca1819b38db01c
dmrangel <outubrodavi@gmail.com>
2026-06-07 00:11:56 -0300

    filmes atualizado pra nova main

--- modules/filme.py
+++ modules/filme.py
@@ -1,9 +1,12 @@
 def buscaFilme(dados, nomeFilme):
-	if dados and nomeFilme:
-		filmes = dados["filmes"]
-		if filmes:
-			for filme in filmes:
-				if filme["nome"] == nomeFilme:
-					return 0, filme
+	if dados:
+		if nomeFilme:
+			filmes = dados["filmes"]
+			if filmes:
+				for filme in filmes:
+					if filme["nome"] == nomeFilme:
+						return 0, filme
+		else:
+			return 0, dados["filmes"]
 	
 	return -1, {}
